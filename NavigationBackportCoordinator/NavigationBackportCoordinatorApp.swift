c5dce9c3176eca243ecf7005e7f65d87f627edff
Igor <chumikov.igor@mail.ru>
2024-02-29 17:07:51 +0700

    Add

---
+++ NavigationBackportCoordinator/NavigationBackportCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBackportCoordinatorApp.swift
+//  NavigationBackportCoordinator
+//
+//  Created by Игорь Чумиков on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBackportCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
